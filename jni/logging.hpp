73921c1deb9dc03c52b8060fc0b1087b5d4b9b8f
Peter Vingelmann <petya@master.com>
2016-09-24 03:46:09 +0200

    No indent for namespaces

--- jni/logging.hpp
+++ jni/logging.hpp
@@ -16,73 +16,73 @@
 
 namespace logging
 {
-    // The message_logger acts as a stream and routes the contents of
-    // the final stream to the Android logcat output.
-    // If ANDROID is not defined, the output is only written to std::cerr.
-    // This class should not be directly instantiated in code,
-    // rather it should be invoked through the LOGW logging macro.
-    class message_logger
+// The message_logger acts as a stream and routes the contents of
+// the final stream to the Android logcat output.
+// If ANDROID is not defined, the output is only written to std::cerr.
+// This class should not be directly instantiated in code,
+// rather it should be invoked through the LOGW logging macro.
+class message_logger
+{
+public:
+
+    message_logger(const char* file, int line, const char* tag,
+                   android_LogPriority severity) :
+        m_file(file), m_tag(tag), m_severity(severity)
     {
-    public:
+        // Prepend the stream with the file and line number.
+        strip_basename(std::string(file), m_filename_only);
+        m_stream << m_filename_only << ":" << line << " > ";
+    }
 
-        message_logger(const char* file, int line, const char* tag,
-                       android_LogPriority severity) :
-            m_file(file), m_tag(tag), m_severity(severity)
+    /// Output the contents of the stream on destruction.
+    ~message_logger()
+    {
+        m_stream << std::endl;
+
+        // Output the log string the Android log at the appropriate level.
+        __android_log_print(m_severity, m_tag.c_str(), m_stream.str().c_str());
+
+        // Indicate termination if needed.
+        if (m_severity == ANDROID_LOG_FATAL)
         {
-            // Prepend the stream with the file and line number.
-            strip_basename(std::string(file), m_filename_only);
-            m_stream << m_filename_only << ":" << line << " > ";
+            __android_log_print(m_severity, m_tag.c_str(), "Terminating.\n");
+            abort();
         }
+    }
 
-        /// Output the contents of the stream on destruction.
-        ~message_logger()
+    // Return the stream associated with the logger object.
+    std::stringstream& stream() { return m_stream; }
+
+private:
+
+    void strip_basename(const std::string& full_path, std::string& filename)
+    {
+        // Try to find the last '/' character
+        const char k_separator = '/';
+        size_t pos = full_path.rfind(k_separator);
+        if (pos != std::string::npos)
         {
-            m_stream << std::endl;
-
-            // Output the log string the Android log at the appropriate level.
-            __android_log_print(m_severity, m_tag.c_str(), m_stream.str().c_str());
-
-            // Indicate termination if needed.
-            if (m_severity == ANDROID_LOG_FATAL)
-            {
-                __android_log_print(m_severity, m_tag.c_str(), "Terminating.\n");
-                abort();
-            }
+            filename = full_path.substr(pos + 1, std::string::npos);
         }
-
-        // Return the stream associated with the logger object.
-        std::stringstream& stream() { return m_stream; }
-
-    private:
-
-        void strip_basename(const std::string& full_path, std::string& filename)
+        else
         {
-            // Try to find the last '/' character
-            const char k_separator = '/';
-            size_t pos = full_path.rfind(k_separator);
+            // Try to find the last '\' character on Windows
+            pos = full_path.rfind('\\');
             if (pos != std::string::npos)
-            {
                 filename = full_path.substr(pos + 1, std::string::npos);
-            }
             else
-            {
-                // Try to find the last '\' character on Windows
-                pos = full_path.rfind('\\');
-                if (pos != std::string::npos)
-                    filename = full_path.substr(pos + 1, std::string::npos);
-                else
-                    filename = full_path;
-            }
+                filename = full_path;
         }
+    }
 
-    private:
+private:
 
-        std::string m_file;
-        std::string m_filename_only;
-        std::string m_tag;
-        std::stringstream m_stream;
-        int m_severity;
-    };
+    std::string m_file;
+    std::string m_filename_only;
+    std::string m_tag;
+    std::stringstream m_stream;
+    int m_severity;
+};
 }
 
 // ---------------------- Macro definitions --------------------------
